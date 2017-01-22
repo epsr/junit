b714279ec9fef0d9c8035daa968140b570e9b7cf
Li Wanghao <li.wanghao@foxmail.com>
2017-01-22 11:05:48 +0800

    Convert to a maven project

--- src/test/java/com/lwh/junit/TestRunner.java
+++ src/test/java/com/lwh/junit/TestRunner.java
@@ -4,6 +4,8 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
+import com.lwh.junit.JunitAnnotation;
+
 public class TestRunner {
 
 	public static void main(String[] args) {
